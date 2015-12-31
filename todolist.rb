8013edc837cdaf14a97458731ceec450bb6404f6
sbower <shawn.bower@gmail.com>
2015-12-31 11:17:31 -0500

    add feature to reorder the list

--- todolist.rb
+++ todolist.rb
@@ -15,19 +15,43 @@ class TodoList
   end
 
   def remove_item(index = 1)
+    valid_index?(index)
     @items.delete_at(index - 1)
   end
 
   def complete(index = 1)
+    valid_index?(index)
     @items[index - 1].completed = true
   end
 
   def uncomplete(index = 1)
+    valid_index?(index)
     @items[index - 1].completed = false
   end
 
+  def move_to_do_up(index)
+    reorder_to_do(index, index - 1)
+  end
+
+  def move_to_do_down(index)
+    reorder_to_do(index, index + 1)
+  end
+
+  def reorder_to_do(from, to)
+    valid_index?(from)
+    valid_index?(to)
+
+    temp = @items[from - 1]
+    @items[from - 1] = @items[to - 1]
+    @items[to - 1]  = temp
+  end
+
+  def to_hash
+    {name: @name, items: @items.map { |item| item.to_hash}}
+  end
+
   def to_json(*args)
-    {name: @name, items: JSON.parse(@items.to_json)}.to_json(*args)
+    to_hash.to_json(*args)
   end
 
   def self.from_json(string)
@@ -50,6 +74,13 @@ class TodoList
       item.print_item
     end
   end
+
+  private
+
+  def valid_index?(index)
+    raise ArgumentError, 'index is out of bounds' unless index > 0 and index <= @items.length
+  end
+
 end
 
 class Item
@@ -74,7 +105,7 @@ class Item
   end
 
   def to_json(*args)
-    self.to_hash.to_json(*args)
+    to_hash.to_json(*args)
   end
 
   def self.from_json(string)
--- udacitask.rb
+++ udacitask.rb
@@ -33,11 +33,35 @@ list.complete(1)
 # Print the list
 list.print_list
 
+# move item in the second position to the first position
+list.move_to_do_up(2)
+
+# Print the list
+list.print_list
+
+puts
+begin
+  # move item to to the first position
+  list.move_to_do_up(1)
+
+  # Print the list
+  list.print_list
+rescue
+  puts "should throw error since we can't move the first item in the list up any futher"
+end
+puts
+
 # Update the title of the list
 list.change_name("New List")
 
 # Print the list
 list.print_list
+
+# export the list object to JSON
+puts
+puts "the following is a json representation of the list"
+puts JSON.dump(list)
+puts
 
 #Build list from JSON
 list2 = TodoList.from_json("{\"name\":\"From List\",\"items\":[{\"description\":\"item5\",\"completed\":true},{\"description\":\"item6\",\"completed\":true}]}")
@@ -50,3 +74,10 @@ list2.uncomplete(2)
 
 # Print the list
 list2.print_list
+
+# Build list from JSON, from the original list, adding a new item to verify
+list.add_item("itemAdded")
+list3 = TodoList.from_json(list.to_json)
+
+# print the list
+list3.print_list
